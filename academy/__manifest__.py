08ffc8ce1a58621d43a056acf2c13188556bdecc
davdenic <davdenic@gmail.com>
2023-08-14 14:25:01 +0000

    [ADD] new module training day 1

---
+++ academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Module for courses and sessions""",
+    'description': """Module to handle:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'David D.',
+    'website': 'www.sozialinfo.ch',
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
