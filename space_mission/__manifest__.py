047f4948fe29a660498d709074269f26090fc2d2
davdenic <davdenic@gmail.com>
2023-08-14 16:17:32 +0000

    [ADD] new module space mission

---
+++ space_mission/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Space Mission',
+    'summary': """Module for space mission logistic""",
+    'description': """Odoo Inc. is trying to visit the Moon. 
+    Module to handle space mission logistic:
+        - Spaceship
+        - Space crew
+        """,
+    'license': 'OPL-1',
+    'author': 'David D.',
+    'website': 'www.sozialinfo.ch',
+    'category': 'Custom Modules',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
