f09db0fd9a4bbb1a24515f55a9e3471b29169398
davdenic <davdenic@gmail.com>
2023-08-15 06:37:44 +0000

    [ADD] new model spaceship fields

--- space_mission/models/spaceship.py
+++ space_mission/models/spaceship.py
@@ -1,6 +1,23 @@
-from odoo import models
+from odoo import fields, models
 
 class Spaceship(models.Model):
     _name = "space_mission.spaceship"
     _description = "Space mission Spaceship"
 
+    name = fields.Char('Ship name', required=True)
+    active = fields.Boolean(default=True)
+    type = fields.Selection(selection=[('freighter', 'Freighter'), 
+                                       ('transport', 'Transport'), 
+                                       ('scout_ship', 'Scout Ship'), 
+                                       ('fighter', 'Fighter')
+                                      ])
+    model = fields.Char(required=True)
+    build_date = fields.Date()
+    captain = fields.Char()
+    required_crew = fields.Integer()
+    length = fields.Float()
+    width = fields.Float()
+    height = fields.Float()
+    engine_number = fields.Char()
+    fuel_type = fields.Selection(selection=[('solid_fuel', 'Solid Fuel'), ('liquid_fuel', 'Liquid Fuel')])
+    
