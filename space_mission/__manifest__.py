7d8babcfdd0dcf4cc765f98a2338ca5e33393f02
davdenic <davdenic@gmail.com>
2023-08-16 12:44:20 +0000

    [ADD] spaceship form view

--- space_mission/__manifest__.py
+++ space_mission/__manifest__.py
@@ -15,7 +15,8 @@
         "security/space_mission_groups.xml",
         "security/ir.model.access.csv",
         "security/space_mission_security.xml",
-        "views/space_mission_menuitems.xml"
+        "views/space_mission_menuitems.xml",
+        "views/spaceship_views.xml",
     ],
     'demo': [
         'demo/spaceship_demo.xml'
